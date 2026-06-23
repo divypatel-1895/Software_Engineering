843b21e0b9afea471bc3b976da2c48dbf1557da6
divypatel-1895 <divypatel2372@gmail.com>
2026-06-23 17:47:41 +0530

    Assignments

---
+++ c/03_Oprators/07_inc_dec.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a = 20;
+	
+	printf("A : %d",a);
+	++a; 
+	printf("\nA : %d",a); 
+	a++; 
+	printf("\nA : %d",a); 
+	
+	printf("\nA : %d",++a); 
+	printf("\nA : %d",a++); 
+	
+	printf("\nA : %d",++a); 
+	printf("\nA : %d",a++); 
+	printf("\nA : %d",a++); 
+	printf("\nA : %d",++a);
+	printf("\nA : %d",a++); 
+	printf("\nA : %d",++a); 
+	printf("\nA : %d",a++); 
+	printf("\nA : %d",++a); 
+	
+	
+	printf("\nA : %d",--a);
+	printf("\nA : %d",a--);
+	printf("\nA : %d",a);
+	
+	
+	
+	getch();
+}
