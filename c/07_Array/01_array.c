5a1cc85378eab6d550c01e4ddaae7c55f0e91041
divypatel-1895 <divypatel2372@gmail.com>
2026-06-27 20:24:37 +0530

    Task

---
+++ c/07_Array/01_array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a[5] = {10,20,30,40,50};
+	
+	printf("a : %d",a[1]);
+	printf("\na : %d",a[3]);
+	
+	
+	getch();
+}
---
+++ c/07_Array/02_all_data.c
@@ -0,0 +1,17 @@
+ #include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a[5] = {10,20,30,40,50};
+	int i;
+	
+    for(i=0;i<5;i++){
+//		printf("%d\n",i);
+//		printf("%d\n",a[i]);
+		printf("a[%d] : %d\n",i,a[i]);
+	}
+	
+	
+	getch();
+}
